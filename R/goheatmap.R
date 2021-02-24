c8986454aaf583c4664538386f95e3764b0d6cc4
ilwookkim <67730977+ilwookkim@users.noreply.github.com>
2021-02-24 22:26:46 +0100

    #

--- R/goheatmap.R
+++ R/goheatmap.R
@@ -5,21 +5,34 @@
 #' @param k number of groups (cutree).
 #' @param n_go number of GO Terms to display.
 #' @param sources Term sources from g:profiler - GO Terms, KEGG, Reactome, WikiPathways, Transfac, miRTarBase, Human Protein Atlas, CORUM protein complexes, Human Phenotype Ontology ("GO:MF","GO:CC","GO:BP","KEGG","REAC","WP","TF","MIRNA","HPA","CORUM","HP")
+#' @param cor  Use correlation coefficient matrix. Default to TRUE.
+#' @param title Title of heatmap
 #' @examples
 #' goheatmap(mat, k = 3, n_go = 3, sources = "GO:BP")
 #' @export
-#' @import ComplexHeatmap gprofiler2 dendextend
+#' @import ComplexHeatmap gprofiler2 dendextend magrittr ellipse
+#' @importFrom dplyr filter top_n
+#' @importFrom grid grid.text grid.rect gpar
 
-goheatmap <- function(mat, k = 3, n_go = 3, sources = "GO:BP"){
+goheatmap <- function(mat, k = 3, n_go = 3, sources = "GO:BP", cor = TRUE, title = "Goheatmap"){
+  if(cor){
+    mat <- mat[, !sapply(mat, function(x) { stats::sd(x) == 0} )]
+    mat <- cor(t(mat), method = "spearman")
+  } else {
+    mat <- data.frame(t(mat))
+  }
+
   ht <- as.dendrogram(hclust(dist(mat)), method = "average")
   ht <- color_labels(ht, k = k)
   ht <- color_branches(ht, k = k)
   labels_ht <- labels_colors(ht)
-
   for (i in 1:k) {
     clusters <- names(labels_ht[which(factor(labels_ht) == unique(labels_ht)[i])])
-    gp <- gost(clusters)
-    gp_mod = gp$result[,c("source", "term_id", "term_name", "p_value")]
+    go <- gost(clusters)
+    if(is.null(go)){
+      print("No information of genes. Check the matirx")
+    }
+    gp_mod = go$result[,c("source", "term_id", "term_name", "p_value")]
     row.names(gp_mod) = gp_mod$term_id
     gp_term <- filter(gp_mod, grepl(sources, source, ignore.case=TRUE))
     n_go_st <- gp_term %>% top_n(-n_go)
@@ -29,19 +42,21 @@ goheatmap <- function(mat, k = 3, n_go = 3, sources = "GO:BP"){
       assign(paste0("lt",i), unlist(lapply(1:n_go, function(x) paste0("NA"))))
     }
   }
-  # text list for GO Terms
   text_list <- lapply(1:k, function(x) get(paste0("lt",x)))
-
   ha = rowAnnotation(foo = anno_empty(border = FALSE,
-                                      width = max_text_width(unlist(text_list)) + unit(1, "mm")))
-
-  Heatmap(cor_df, name = "cor_df", cluster_rows = ht, row_split = k,
-          column_split = k,  right_annotation = ha,border = TRUE , show_row_names = FALSE, show_column_names = FALSE)
+                                      width = max_text_width(unlist(text_list)) + unit(4, "mm")))
+  draw(Heatmap(mat, name = title,
+               cluster_rows = ht,
+               row_split = k,
+               column_split = k,
+               right_annotation = ha,
+               border = TRUE ,
+               show_row_names = FALSE,
+               show_column_names = FALSE,
+               column_title = NULL,
+               row_title = NULL))
   for(i in 1:k) {
     decorate_annotation("foo", slice = i, {
-      grid.rect(x = 0, width = unit(1, "mm"), gp = gpar(fill = i, col = NA), just = "left")
-      grid.text(paste(text_list[[i]], collapse = "\n"), x = unit(1, "mm"), just = "left")
-    })
-  }
-
+      grid.text(paste(text_list[[i]], collapse = "\n"), x = unit(2, "mm"), just = "left")
+    })}
 }
--- README.md
+++ README.md
@@ -18,30 +18,40 @@ library(GOheatmap)
 **Load example data**
 
 ``` r 
-mat.file <- system.file("extdata", "countdata_examples.Rdata", package="GOheatmap")
+mat.file <- system.file("extdata", "mat.Rdata", package="GOheatmap")
 load(mat.file)
 ```
 
 **Pre-treatment**
 
-Remove NAs. For the correlation coefficient matrix, remove values which has zero variance.
+Remove NA, if necessary.
 
 ``` r
 
 mat <- data.frame(na.omit(mat))
-# Matrix transpose
-mat <- data.frame(t(mat))
-mat <- mat[, !sapply(mat, function(x) { sd(x) == 0} )]
-cor_df <- cor(t(mat), method = "spearman")
-                           
+knitr::kable(head(mat[, 1:4], 3), "simple")
+
+```
+
+``` r
+        TCGA.AB.2803.03   TCGA.AB.2805.03   TCGA.AB.2806.03   TCGA.AB.2807.03
+-----  ----------------  ----------------  ----------------  ----------------
+AGO1          1758.2305         4728.2463         2947.8039         2831.1817
+AGO2           367.2840          771.7537         1113.3036         1076.4647
+AGO3           727.3663         1237.6171          896.2444          965.2433
 ```
 
 **Run goheatmap**
 
-Parameters k (number of clustering), n_go (number of terms to display), sources (details here)[https://biit.cs.ut.ee/gprofiler/page/apis]
+Parameters k (number of clustering), n_go (number of terms to display), sources [details here](https://biit.cs.ut.ee/gprofiler/page/apis)
+
 ``` r
-goheatmap(mat, k = 3, n_go = 3, sources = "GO:BP")
+goheatmap(mat, k = 3, n_go = 3, sources = "GO:BP", cor = TRUE, title = "GOheatmap")
+goheatmap(mat, k = 3, n_go = 3, sources = "KEGG", cor = TRUE, title = "GOheatmap")
+goheatmap(mat, k = 3, n_go = 3, sources = "WP", cor = TRUE, title = "GOheatmap")
 
 ```
 
-<img src="inst/extdata/example.png"/>
+<img src="inst/extdata/example_go.bp.png"/>
+<img src="inst/extdata/example_kegg.png"/>
+<img src="inst/extdata/example_wp.png"/>
